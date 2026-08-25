5702531b3ae7e473d53da2c647189aebcc54207c
Baotran07-zip <bao.tran.073108@gmail.com>
2026-08-25 21:48:43 +0700

    commit day 4

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -1,24 +1,23 @@
-from fastapi import Depends,HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
-from jose import jwt,JWTError
+from jose import jwt, JWTError
 
 from sqlalchemy.orm import Session
 
 from app.db.database import get_db
 from app.models.users import User
-from app.core.security import SECRET_KEY,ALGORITHM
+from app.core.security import SECRET_KEY, ALGORITHM
 
 
-oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="/auth/login"
-)
+security = HTTPBearer()
 
 
 def get_current_user(
-    token: str = Depends(oauth2_scheme),
+    credentials: HTTPAuthorizationCredentials = Depends(security),
     db: Session = Depends(get_db)
 ):
+    token = credentials.credentials
 
     try:
         payload = jwt.decode(
@@ -35,7 +34,7 @@ def get_current_user(
                 detail="Token không hợp lệ"
             )
 
-    except (JWTError,ValueError):
+    except (JWTError, ValueError):
         raise HTTPException(
             status_code=401,
             detail="Token không hợp lệ hoặc đã hết hạn"
@@ -63,7 +62,6 @@ def get_current_user(
 def require_admin(
     current_user: User = Depends(get_current_user)
 ):
-
     if current_user.role != "ADMIN":
         raise HTTPException(
             status_code=403,
